f9463d9952d4e5a6f7bceae58a906a2bb5da5918
Jakyp-05 <jakypjumagulov080gmail.com>
2023-11-06 12:19:24 +0600

    added new file

---
+++ index.js
@@ -0,0 +1,50 @@
+let products = [
+    {
+        id: 1,
+        image: "./assets/images/1.png",
+        title: "",
+        price: "",
+    },
+    {
+        id: 2,
+        image: "./assets/images/2.png",
+        title: "",
+        price: "",
+    },
+    {
+        id: 3,
+        image: "./assets/images/3.png",
+        title: "",
+        price: "",
+    },
+    {
+        id: 4,
+        image: "./assets/images/4.png",
+        title: "",
+        price: "",
+    },
+    {
+        id: 5,
+        image: "./assets/images/5.png",
+        title: "",
+        price: "",
+    },
+    {
+        id: 6,
+        image: "./assets/images/6.png",
+        title: "",
+        price: "",
+    },
+    {
+        id: 7,
+        image: "./assets/images/7.png",
+        title: "",
+        price: "",
+    },
+    {
+        id: 8,
+        image: "./assets/images/8.png",
+        title: "",
+        price: "",
+    },
+];
